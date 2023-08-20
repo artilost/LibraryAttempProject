3bc9b7657a89a8f4922d3dd9120a089e563ce51f
Artilost <mustafakalender2000@gmail.com>
2023-08-20 21:32:19 +0300

    Fisrt

---
+++ LibraryProject/Library.Entities/Concrete/Members.cs
@@ -0,0 +1,13 @@
+﻿using Library.Entities.Abstruct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Entities.Concrete
+{
+    public class Members:IEntity
+    {
+    }
+}
